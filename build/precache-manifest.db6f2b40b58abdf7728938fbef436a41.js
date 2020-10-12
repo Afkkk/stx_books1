a9cf98e3c4d6e8de149df3a333ff2b34cc76c84e
AFKKK <kklaptocz1617@gmail.com>
2020-10-12 15:05:00 +0200

    part1

---
+++ build/precache-manifest.db6f2b40b58abdf7728938fbef436a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f22597a9bf2f48f7fd8cc5386222b7",
+    "url": "/stx_books1/index.html"
+  },
+  {
+    "revision": "41426b5d76312a0fcfa3",
+    "url": "/stx_books1/static/css/main.c5e68773.chunk.css"
+  },
+  {
+    "revision": "e389bd59fbee89891280",
+    "url": "/stx_books1/static/js/2.e7cad2a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stx_books1/static/js/2.e7cad2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41426b5d76312a0fcfa3",
+    "url": "/stx_books1/static/js/main.b60fbbdf.chunk.js"
+  },
+  {
+    "revision": "240e2beb5e1e23ae66ef",
+    "url": "/stx_books1/static/js/runtime-main.01b6ca7b.js"
+  }
+]);
